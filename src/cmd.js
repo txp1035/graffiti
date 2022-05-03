07906c66742393c5f5540870bea52c52ccb647b3
DaoYuan <txp1035@gmail.com>
2022-05-03 11:46:59 +0800

    feat: add function

--- src/cmd.js
+++ src/cmd.js
@@ -3,13 +3,15 @@
  * 开始时间
  * 结束时间
  */
-
+const startTime = '2017-04-30';
+const endTime = '2022-04-14';
 /**
  * 方法区域
  * 提交代码方法
  * 随机返回01方法
+ * 基于开始结束时间生成日期
  */
-
+function createDateArr(startTime, endTime) {}
 /**
  * 执行区域
  */
